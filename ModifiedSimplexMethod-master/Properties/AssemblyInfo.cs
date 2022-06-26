0c8fd81492ceb991249e9f27a43197fa4a15e4c6
Gaspar Gomez <77340598+ggomez0@users.noreply.github.com>
2022-06-26 14:49:28 -0300

    Add files via upload

---
+++ ModifiedSimplexMethod-master/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Resources;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Sobre nosotros")]
+[assembly: AssemblyDescription("Un programa para calcular el metodo simplex revisado a partir de ingresar la funcion objetivo y sus restricciones")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Gaspar Gomez y Facundo Visa")]
+[assembly: AssemblyProduct("Metodo Simplex Revisado")]
+[assembly: AssemblyCopyright("")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("d8e4def0-ce01-430f-b074-5774c753ef10")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: NeutralResourcesLanguage("es-AR")]
